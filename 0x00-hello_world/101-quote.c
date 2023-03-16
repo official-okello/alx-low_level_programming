48e0c4c937c67860d6716c567a8857ca3781a9b8
official-okello <officialokello@gmail.com>
2023-03-16 17:47:31 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	printf("and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n");
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str1, 59);
+
 	return 1;
 }
