03e70e1492bc4b40bf2e18a14b75ae45903c0ef4
official-okello <officialokello@gmail.com>
2023-03-16 13:52:33 +0300

    All tasks first attempt

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %lu  byte(s), sizeof(c)\n");
+	printf("Size of an int: %lu  byte(s), sizeof(int)\n");
+	printf("Size of a long int: %lu byte(s), sizeof(long int)\n");
+	printf("Size of a long long int: %lu byte(s), sizeof(long long int)\n");
+	printf("Size of a float: %lu byte(s), sizeof(float)\n");
+
+	return 0;
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions to 0x00. C - Hello, World project
