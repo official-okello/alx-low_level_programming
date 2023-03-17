dd6a518d1a4284727a5766480603abd1d97d35cf
official-okello <officialokello@gmail.com>
2023-03-17 23:56:07 +0300

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	}
 	else if (m == 0)
 	{
-		printf("Last digit of %d is %d and is greater than 0\n", n, m);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
 	}
 	return (0);
 }
