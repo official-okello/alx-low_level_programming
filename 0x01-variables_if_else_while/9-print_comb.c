a42180bc37041f9f1d03b2812a704a3036231ad5
official-okello <officialokello@gmail.com>
2023-03-18 00:51:31 +0300

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 48; i < 58; i++)
 	{
@@ -15,7 +16,11 @@ int main(void)
 		{
 			putchar(i);
 			putchar(j);
-			putchar(', ');
+			if (j == 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	return (0);
