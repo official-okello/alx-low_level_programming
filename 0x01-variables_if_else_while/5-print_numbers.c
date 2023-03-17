98f1e517fc0e7b2488d2669ff223e9f3a65a78d7
official-okello <officialokello@gmail.com>
2023-03-18 00:28:07 +0300

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	char num[10] = '0123456789';
+	char d[10] = '0123456789';
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(num[i]);
+		putchar(d[i]);
 	}
 	putchar('\n');
 	return (0);
