9b898d2fda86886e3a3d6e50530cf853963d870d
official-okello <officialokello@gmail.com>
2023-03-18 00:02:19 +0300

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
-
+	int i;
 	for (i == 0, i < 26, i++)
 	{
-		putchar(alph[i];
+		putchar(alph[i]);
 	}
 	putchar("\n");
 	return (0);
