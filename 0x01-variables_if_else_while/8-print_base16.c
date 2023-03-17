a74f9cf6e75387c9a5520f5e9cff4bc20fdb3022
official-okello <officialokello@gmail.com>
2023-03-18 00:45:31 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 48, i < 58, i++)
+	for (i = 48; i < 58; i++)
 	{
-		putachar(i);
+		putchar(i);
 	}
-	for (i = 97, i < 103, i++)
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
