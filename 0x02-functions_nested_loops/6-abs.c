4fe8e02b48a475aa01885be98ab67e4a4b5e6ef1
official-okello <officialokello@gmail.com>
2023-03-21 18:22:59 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @r: integer being converted to absolute
+ *
+ * Return: 0.
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+		return (r);
+	else
+		return (r * -1);
+}
