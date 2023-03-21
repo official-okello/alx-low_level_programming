b1a264c24142cd0bbd2e71d52669e1b2c5b41385
official-okello <officialokello@gmail.com>
2023-03-21 21:41:46 +0300

    8-24_hours.c quick fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,14 @@ void jack_bauer(void)
 
 	for (w = 48; w <= 50; w++)
 	{
-		for (x = 48; x <= 51; x++)
+		for (x = 48; x <= 57; x++)
 		{
 			for (y = 48; y <= 53; y++)
 			{
 				for (z = 48; z <= 57; z++)
 				{
+					if (w = 50 && x >= 52)
+						break;
 					_putchar(w);
 					_putchar(x);
 					_putchar(58);
