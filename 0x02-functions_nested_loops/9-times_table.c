1ff0b2ae524a2d19939f64f0075d73395158c966
official-okello <officialokello@gmail.com>
2023-03-21 22:55:51 +0300

    9-times_table.c fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,33 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= 9; j++)
 		{
-			return (i * j);
+			k = (i * j);
+			if ((k / 10) > 0)
+			{
+				_putchar((k / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((k % 10) + '0');
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
+
 }
