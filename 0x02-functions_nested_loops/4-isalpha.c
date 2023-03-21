16477613683d35c5de4aa62bc08296cbddc72cde
official-okello <officialokello@gmail.com>
2023-03-21 17:48:53 +0300

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/**
+ * _isalpha - checks for alphabet
+ *
+ * @c: The ascii code of the character
+ *
+ * Return: 1 if true and 0 if false.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c < 123)
+		return (1);
+	else if (c >= 65 && c < 91)
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
