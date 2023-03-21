0c257940fcb036fb107e02554b2577563fb1ed64
official-okello <officialokello@gmail.com>
2023-03-21 21:56:43 +0300

    9-times_table.c added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: None.
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			return (i * j);
+		}
+		_putchar('\n');
+	}
+}
