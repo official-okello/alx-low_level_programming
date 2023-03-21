6e10bfbd5c7a6a79169b3dffaf45419f29a3dbb6
official-okello <officialokello@gmail.com>
2023-03-21 18:06:57 +0300

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - check for positive, negative, zero
+ *
+ * @n: integer being checked
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+	}
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+	_putchar('\n');
+}
