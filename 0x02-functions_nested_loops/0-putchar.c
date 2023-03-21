05fef9383c1012aad2df2e33df9e60311d17e71e
official-okello <officialokello@gmail.com>
2023-03-21 16:49:51 +0300

    fix 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,9 @@
  * Return: Always 0 (Success)
  */
 
- int main(void)
- {
- 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count = 0, c_size;
 
 	c_size = sizeof(str) / sizeof(int);
