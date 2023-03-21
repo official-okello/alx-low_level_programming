46222b8ba213adca4f5a910ad8af2b4b7c16c0f1
official-okello <officialokello@gmail.com>
2023-03-21 17:39:27 +0300

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_islower - checks for lowercase character
+ *
+ *@c: The ascii code of the character
+ *
+ * Return: 1 if true and 0 if false.
+ */
+
+int _islower(int c)
+{
+	int c;
+	int return_value;
+
+	if (c >= 97 && c < 123)
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
