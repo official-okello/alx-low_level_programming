453596847802c9c942ca5c658a47b0fea7d12d65
official-okello <officialokello@gmail.com>
2023-03-21 17:10:26 +0300

    Quick fix 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
  *
  * print_alphabet - prints the alphabet in lowercase
  *
- * Return: Always 0.
- */
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
- * print_alphabet - Prints the alphabet in lowercase.
+ * Return: None.
  */
 
 void print_alphabet(void)
