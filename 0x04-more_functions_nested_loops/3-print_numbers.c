3ba6e0d1400a13134d70128ef09f286f33fc81ec
official-okello <officialokello@gmail.com>
2023-03-23 07:14:50 +0300

    3-print_numbers.c added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - prints 0 to 9
+*@c: ascii of number
+*Return: None
+*/
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c >= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
