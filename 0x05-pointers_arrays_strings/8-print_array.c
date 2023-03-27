dc05b97d666437d45455038030ecbc02659baa69
official-okello <officialokello@gmail.com>
2023-03-27 23:54:33 +0300

    8-print_array.c quick fix

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_array - prints n number of elements of an array
+*@a: pointer to array
+*@n: number of elements
+*/
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (x != n - 1)
+		{
+			printf("%d, ", a[x]);
+		}
+		else
+		{
+			printf("%d", a[x]);
+		}
+	}
+}
