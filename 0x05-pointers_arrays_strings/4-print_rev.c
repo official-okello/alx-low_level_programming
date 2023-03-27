a8f51f06f839a97e69cf15a297cba21d7e02e6e2
official-okello <officialokello@gmail.com>
2023-03-28 01:01:06 +0300

    4-print_rev.c quick fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*print_rev - prints a string in reverse
-*@s: string
-*/
-
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
 void print_rev(char *s)
 {
-	for (x in s)
+	int c = 0;
+
+	while (s[c] != '\0')
 	{
-		_putchar(s[-x]);
+		c++;
 	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
 	_putchar('\n');
 }
+
