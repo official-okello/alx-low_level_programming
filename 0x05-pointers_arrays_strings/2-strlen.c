26c850a6d5a64329c3627b0becf98e9b57035c19
official-okello <officialokello@gmail.com>
2023-03-27 22:04:26 +0300

    2-strlen.c added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_strlen - prints length of a string
+*@s: string
+*Return: integer, length of string s
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	for (x in s)
+	{
+		count++;
+	}
+	return (count);
+}
