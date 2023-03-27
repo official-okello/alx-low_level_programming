97a607e1a0335b9aba4a4c07955a76817535892f
official-okello <officialokello@gmail.com>
2023-03-27 22:26:26 +0300

    3-puts.c added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_puts - prints a string followed by a new line
+*@str:strinf to be printed
+*/
+
+void _puts(char *str)
+{
+	for (x in str)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
