4ffd8f625c8efc8800de8c31f614c7ed014bc73c
official-okello <officialokello@gmail.com>
2023-03-28 00:38:15 +0300

    7-puts_half.c quick fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,16 +13,16 @@ void puts_half(char *str)
 	{
 		x++;
 	}
-	if (count %  2 != 0)
+	if (x % 2 != 0)
 	{
-		y = (count - 1) / 2;
+		y = (x - 1) / 2;
 		y += 1;
 	}
 	else
 	{
-		y = count / 2;
+		y = x / 2;
 	}
-	for (; y < count; y++)
+	for (; y < x; y++)
 	{
 		_putchar(str[y]);
 	}
