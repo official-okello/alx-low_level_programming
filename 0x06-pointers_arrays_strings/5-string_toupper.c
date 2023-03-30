f009d0b01311f04e22ac34fca8e71df33eb67dd6
official-okello <officialokello@gmail.com>
2023-03-30 22:56:36 +0300

    5-string_toupper.c quick fix

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * string_toupper - lowercase letters to uppercase
+  * @p: string to be modified
+  *
+  * Return: char
+  */
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (p);
+}
+
