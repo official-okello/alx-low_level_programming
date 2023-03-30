01753b615ddcfef79b304524ccb0321dfd3a0ad5
official-okello <officialokello@gmail.com>
2023-03-30 22:51:47 +0300

    3-strcmp.c quick fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,50 @@
 #include "main.h"
 
 /**
-*_strcmp - compares two strings
-*@s1: first string
-*@s2: second string
-*Return: integer value
-*/
-
+  * _strcmp - Compares two strings
+  * @s1: first string
+  * @s2: second string
+  *
+  * Return: integer value
+  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1len = 0, s2len = 0;
+	int a = 0, b = 0, c = 0, r = 0, lim;
 
-	while (s1[s1len] != '\0')
+	while (s1[a])
 	{
-		s1len++;
+		a++;
 	}
-	while (s2[s2len] != '\0')
+
+	while (s2[b])
 	{
-		s2len++;
+		b++;
 	}
-	if (s1len > s2len)
-		return (1);
-	else if (s2len > s1len)
-		return (-1);
+
+	if (a <= b)
+	{
+		lim = a;
+	}
 	else
-		return (0);
+	{
+		lim = b;
+	}
+
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+
+	return (r);
 }
