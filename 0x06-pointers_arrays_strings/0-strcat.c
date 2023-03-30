7b4deb9bdaf26875f14acf943220242bba6a47f4
official-okello <officialokello@gmail.com>
2023-03-30 19:53:44 +0300

    0-strcat.c quick fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,25 +2,27 @@
 #include <stdio.h>
 
 /**
-*_strcat - concatenates two strings
-*@dest: concatenate to
-*@src: concatenate from
-*Return: concatenateted dest
-*/
-
+  * _strcat - Concatenates two strings
+  * @dest: The destination string
+  * @src: The source string
+  *
+  * Return: A pointer to the resulting string dest
+  */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j;
 
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}
-	for (j = 0; src[j] != '\0'; j++)
+
+	for (j = 0; src[j] != 0; j++)
 	{
 		dest[i] = src[j];
-		i++
+		i++;
 	}
-	_putchar('\0');
+
+	dest[i] = '\0';
 	return (dest);
 }
