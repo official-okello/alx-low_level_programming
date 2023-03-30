5e2d4b211c69cd0a1391ae448efaebc116c4f331
official-okello <officialokello@gmail.com>
2023-03-30 20:02:12 +0300

    1-strncat.c added

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strncat - concatenates strings
+*@dest: concatenate to
+*@src: concatenate from
+*@n: bytes
+*Return: Concatenate string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dlen] = src[i];
+		dest++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
