2013f526ac093f2f119a4eea305e114dc8e3665a
official-okello <officialokello@gmail.com>
2023-03-30 10:53:17 +0300

    0-strcat.c, main.h and READme.md added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_strcat - concatenates two strings
+*@dest: concatenate to
+*@src: concatenate from
+*Return: concatenateted dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] == src[j];
+		i++;
+		j++;
+	}
+	_putchar('\0');
+	return (dest);
+}
