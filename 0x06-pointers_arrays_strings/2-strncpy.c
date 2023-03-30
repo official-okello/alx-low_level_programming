891b4ce6689746c4a2f9341b417a662a7c5b9b38
official-okello <officialokello@gmail.com>
2023-03-30 20:36:01 +0300

    2-strncpy.c added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strncpy - copies string
+*@dest: copy to
+*@src: copy from
+*@n: bytes
+*Return: copied string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i + 1] = '\0';
+	return (dest);
+}
