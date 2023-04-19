d5abd8fc42757dc56765706e429ffba9a07b70e5
official-okello <officialokello@gmail.com>
2023-04-19 19:50:56 +0300

    3-op_functions.c quick fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,50 +1,48 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * op_add - adds 
-  * @a: integer
-  * @b: integer
-  *
-  * Return: a + b
-  */
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: add.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-  * op_sub - subtracts
-  * @a: int
-  * @b: int
-  *
-  * Return: a - b
-  */
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
-  * op_mul - multiplies
-  * @a: int
-  * @b: int
-  *
-  * Return: a * b
-  */
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
-  * op_div - divides
-  * @a: int
-  * @b: int
-  *
-  * Return: a / b
-  */
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -52,17 +50,16 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a / b);
 }
 
 /**
-  * op_mod - modulus
-  * @a: int
-  * @b: int
-  *
-  * Return: a % b
-  */
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
@@ -70,6 +67,5 @@ int op_mod(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a % b);
 }
