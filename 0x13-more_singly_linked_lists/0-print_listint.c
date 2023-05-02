37b03841a7e5662c73d13dab18814b6a08cf65a9
official-okello <officialokello@gmail.com>
2023-05-02 18:45:20 +0300

    0-print_listint.c quick fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
  * print_listint - prints all the elements of a list.
  * @h: head of a list.
  *
- * Return: numbers of nodes.
+ * Return: the number of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
