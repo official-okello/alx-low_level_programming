8803238c16d3e0c0372e971ada10d66817badb26
official-okello <officialokello@gmail.com>
2023-08-23 21:22:53 +0300

    5-island_perimeter.py correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" Island Perimeter """
 
 
 def island_perimeter(grid):
-    """ Island Perimeter function """
-    peri = 0
-    gl = []
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            gl.append(grid[i][j])
-    for i in range(len(gl)):
-        if i < len(gl) - 1 and i > 0:
-            if gl[i] == 0 and gl[i + 1] == 1 or gl[i] == 0 and gl[i - 1] == 1:
-                peri += 1
-    if peri % 2 == 0:
-        peri *= 2
-    else:
-        peri = (peri * 2) - 1
-    return peri
+    """
+    Method island_perimeter:
+    returns the perimeter of the island described in grid
+    """
+    perim = 0
+    for row in range(len(grid)):
+        for colum in range(len(grid[row])):
+            if grid[row][colum] == 1:
+                if row == 0 or grid[row - 1][colum] == 0:
+                    perim += 1
+                if colum == 0 or grid[row][colum - 1] == 0:
+                    perim += 1
+                if row == len(grid) - 1 or grid[row + 1][colum] == 0:
+                    perim += 1
+                if colum == len(grid[row]) - 1 or grid[row][colum + 1] == 0:
+                    perim += 1
+    return perim
